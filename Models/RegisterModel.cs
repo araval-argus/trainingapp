977f30c68e55da473221c3738b219e0820984fa2
Ankur@Laptop <avkhunt007@gmail.com>
2023-04-04 18:10:55 +0530

    Added Length Attribute to validate length

--- Models/RegisterModel.cs
+++ Models/RegisterModel.cs
@@ -12,15 +12,22 @@ namespace ChatApp.Models
         //Also We can add RegularExpressionAttribute to validate password strength
 
         [Required(ErrorMessage = "Enter First Name")]
+        [StringLength(20, MinimumLength =3, ErrorMessage ="First Name Should Be between length of 3-50")]
         public string FirstName { get; set; }
         [Required(ErrorMessage = "Enter Last Name")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Last Name Should Be between length of 3-50")]
+
         public string LastName { get; set; }
         [Required(ErrorMessage = "Enter UserName")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "UserName Should Be between length of 3-50")]
+
         public string UserName { get; set; }
         [Required(ErrorMessage = "Enter Email Address")]
         [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Email Should Be between length of 3-50")]
         public string Email { get; set; }
         [Required(ErrorMessage = "Enter Password")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Password Should Be between length of 3-50")]
         public string Password { get; set; }
     }
 }
