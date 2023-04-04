971d4a13a592768bfd20378f0a33a93d993e6392
Ankur@Laptop <avkhunt007@gmail.com>
2023-04-04 18:04:31 +0530

    Added Backend Validation by adding

--- Models/LoginModel.cs
+++ Models/LoginModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,14 @@ namespace ChatApp.Models
 {
     public class LoginModel
     {
+        //Added attribute to validate input && Specially Email
+        //Also We can add RegularExpressionAttribute to validate password strength
+        [Required(ErrorMessage = "Invalid Credential")]
         public string Username { get; set; }
+        [Required(ErrorMessage = "Invalid Credential")]
+        [EmailAddress(ErrorMessage = "Enter Valid Email")]
         public string EmailAddress { get; set; }
+        [Required(ErrorMessage = "Invalid Credential")]
         public string Password { get; set; }
     }
 }
--- Models/RegisterModel.cs
+++ Models/RegisterModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,19 @@ namespace ChatApp.Models
 {
     public class RegisterModel
     {
+        //Added attribute to validate input && Specially Email
+        //Also We can add RegularExpressionAttribute to validate password strength
+
+        [Required(ErrorMessage = "Enter First Name")]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "Enter Last Name")]
         public string LastName { get; set; }
+        [Required(ErrorMessage = "Enter UserName")]
         public string UserName { get; set; }
+        [Required(ErrorMessage = "Enter Email Address")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Enter Password")]
         public string Password { get; set; }
     }
 }
